eeac4f90aa2f0701a1596c04cf75a743bc6db24f
fernando <fernando_lopezc@live.com>
2019-02-09 17:49:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,3 +25,25 @@ end
 def shift_with_args(arr)
   brands_removed = arr.shift(2)
 end
+
+def using_concat(arr1,arr2)
+  arr1.concat(arr2)
+  end
+
+def using_insert
+end
+
+def using_uniq
+  
+end
+
+def using_flatten
+  
+end
+
+def using_delete
+end
+
+def using_delete_at
+  
+end
